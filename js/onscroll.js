d2b185975ef2038f409c83b9a332d26b51df4f3a
Muthu Kumar <88616865+mk625@users.noreply.github.com>
2024-06-21 23:03:57 +0530

    code update

--- js/onscroll.js
+++ js/onscroll.js
@@ -1,30 +1,24 @@
 
 
-$(window).on('scroll', function () {
+$(window).on('wheel', function (event) {
     let windowHeight = $(window).height();
     let scrollTop = $(this).scrollTop();
+    let scrollToBottom = Math.sign(event.originalEvent.deltaY) === 1;
+
+    console.log(scrollToBottom);
 
     // book container
     let book_container = $("#timeline-book-container");
     let rotatex = $('#timeline-book-container').attr('data-rotatex');
     // \\\ book container
 
-    console.log(rotatex);
-
-    $(book_container).css('transform', 'perspective(800px) rotateX(' + (rotatex - 1) + 'deg)');
-    $('#timeline-book-container').attr('data-rotatex', rotatex - 1);
-
-
-    // if ((scrollTop > ($(book_container).offset().top) - (windowHeight/2)) && (rotatex >= 0)) {
-    //     $(book_container).css('transform', 'perspective(800px) rotateX(' + (rotatex - 1) + 'deg)');
-    // } else if ((scrollTop < ($(book_container).offset().top)  - (windowHeight/2))) {
-    //     console.log("else if");
-    //     $(book_container).css('transform', 'perspective(800px) rotateX(' + (rotatex + 1) + 'deg)');
-    // }
-
-    $("body").attr("data-scroll-val", scrollTop);
-
+    if ((scrollToBottom) && (scrollTop > ($(book_container).offset().top) - (windowHeight/2)) && (rotatex > 0)) {
+        $(book_container).css('transform', 'perspective(800px) rotateX(' + (rotatex - 1) + 'deg)');
+        $('#timeline-book-container').attr('data-rotatex', rotatex - 1);
+    } else if ((!scrollToBottom) && (scrollTop < ($(book_container).offset().top) - (windowHeight / 2)) && (rotatex < 0)) {
+        $(book_container).css('transform', 'perspective(800px) rotateX(' + (rotatex - 1) + 'deg)');
+        $('#timeline-book-container').attr('data-rotatex', rotatex - 1);
+    }
 });
-
 
 
