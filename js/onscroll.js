3c81939f40b8eb4da66a15651b40effd9c9775a7
Muthu Kumar <88616865+mk625@users.noreply.github.com>
2024-06-30 18:31:20 +0530

    code update

--- js/onscroll.js
+++ js/onscroll.js
@@ -1,24 +1,37 @@
 
 
 $(window).on('wheel', function (event) {
-    let windowHeight = $(window).height();
-    let scrollTop = $(this).scrollTop();
-    let scrollToBottom = Math.sign(event.originalEvent.deltaY) === 1;
+    // book section
+        let windowHeight = $(window).height();
+        let scroll_val = $(this).scrollTop();
+        let scrollToBottom = Math.sign(event.originalEvent.deltaY) === 1;
+        
+        let book_container = $("#timeline-book-container");
+        let book_top = $(book_container).offset().top;
+        let rotatex = $(book_container).attr('data-rotatex');
 
-    console.log(scrollToBottom);
+        // console.log(scroll_val);
+        // console.log(windowHeight - $(book_container).height());
 
-    // book container
-    let book_container = $("#timeline-book-container");
-    let rotatex = $('#timeline-book-container').attr('data-rotatex');
-    // \\\ book container
 
-    if ((scrollToBottom) && (scrollTop > ($(book_container).offset().top) - (windowHeight/2)) && (rotatex > 0)) {
-        $(book_container).css('transform', 'perspective(800px) rotateX(' + (rotatex - 1) + 'deg)');
-        $('#timeline-book-container').attr('data-rotatex', rotatex - 1);
-    } else if ((!scrollToBottom) && (scrollTop < ($(book_container).offset().top) - (windowHeight / 2)) && (rotatex < 0)) {
-        $(book_container).css('transform', 'perspective(800px) rotateX(' + (rotatex - 1) + 'deg)');
-        $('#timeline-book-container').attr('data-rotatex', rotatex - 1);
-    }
+        if ((scrollToBottom) && (scroll_val > (book_top) - (windowHeight / 2)) && (rotatex > 0)) {
+            console.log("if");
+            $(book_container).css('transform', 'perspective(800px) rotateX(' + (rotatex - 1) + 'deg)');
+            $(book_container).attr('data-rotatex', rotatex - 1);
+        } else if ( (!scrollToBottom) && (scroll_val < (book_top)) && (rotatex < 70) ) {
+            console.log("if else");
+            $(book_container).css('transform', 'perspective(800px) rotateX(' + (rotatex + 1) + 'deg)');
+            $(book_container).attr('data-rotatex', rotatex + 1);
+        }
+    // \\\ book section
+
+
+
+    // skills section
+        if ((scroll_val + 250) > ($("#features-sec").offset().top)) {
+            $("#features-sec").addClass("features-sec-anim");
+        }
+    // \\\ skills section
 });
 
 
